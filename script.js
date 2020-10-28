8c8b38d0e2191e8c9b8c5060d55d7f29390f8bc5
S Galarza <sgalarza4190@gmail.com>
2020-10-28 15:43:47 -0400

    fixed character loop

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Array of possible characters
 var symbolChar = ["!", "@", "#", "$", "%", "^", "&", "*", "<",">", "~"];
 var numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var finalPass = [" "]
 var allChar =["symbolChar", "numberChar","lowerChar","upperChar"];
 
 // Assignment Code
@@ -30,15 +30,15 @@ if (wantPass === true){
     var inputPass = prompt("How many characters would you like your password to be?");
   }
   
+  for (allChar === false; !passSymbol && !passNumber && !passLower && !passUpper; allChar++) {
+    alert("Please choose at least one type of character");
+    var passSymbol = confirm("Would you like symbol characters in your password?");
+    var passNumber = confirm("Would you like number characters in your password?");
+    var passLower = confirm("Would you like lowercase characters in your password?");
+    var passUpper = confirm("Would you like uppercase characters in your password?");
+  }
+
+
   //testing prompt
   // alert(" code is " + inputPass);
-  
-  // else {
-  //   passSymbol = confirm("Would you like symbol characters in your password?");
-  //   passNumber = confirm("Would you like number characters in your password?");
-  //   passLower = confirm("Would you like lowercase characters in your password?");
-  //   passUpper = confirm("Would you like uppercase characters in your password?");
-  // }
-  // if (!passSymbol && !passNumber && !passLower && !passUpper) {
-  //   alert("Please choose at least one type of character");
   }
